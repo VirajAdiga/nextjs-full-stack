ddfc15f4c9fc1c526540a7a444234cc568d53c1f
VirajAdiga <virajadigamaclub@gmail.com>
2024-12-02 14:56:20 +0530

    client side vs server side rendering

---
+++ next-js-app/app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddToCart from './AddToCart'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <div>ProductName</div>
+        <AddToCart/>
+    </div>
+  )
+}
+
+export default ProductCard
--- next-js-app/app/page.tsx
+++ next-js-app/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
@@ -10,8 +11,10 @@ export default function Home() {
       <a href='/users'>Users (Server side render)</a>
       <br/>
 
-      {/* This performs client side rendering, optimal when repeatative loading takes not relevant (css, js, etc) */}
+      {/* This performs client side rendering, optimal when repeatitive loading is not relevant */}
       <Link href={'/users'}>Users (Client side render)</Link>
+
+      <ProductCard />
     </main>
   )
 }
