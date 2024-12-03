a0c47018fc4317d882f20cf572d00821fa6fbc4b
VirajAdiga <virajadigamaclub@gmail.com>
2024-12-03 15:51:11 +0530

    Add daisy ui for styling

--- next-js-app/app/users/page.tsx
+++ next-js-app/app/users/page.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 interface User {
   id: number,
-  name: string
+  name: string,
+  email: string
 }
 
 const usersPage = async () => {
@@ -15,9 +16,20 @@ const usersPage = async () => {
     <div>
       <h1>Users</h1>
       <div>{new Date().toLocaleString()}</div>
-      <ul>
-        {users.map(user => <li key={user.id}>{user.name}</li>)}
-      </ul>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>)}
+        </tbody>
+      </table>
     </div>
   )
 }
