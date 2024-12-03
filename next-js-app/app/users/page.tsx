f3007dd9c1138a701c3e87a578ea625cd0231d4d
VirajAdiga <virajadigamaclub@gmail.com>
2024-12-03 14:52:40 +0530

    static vs dynamic rendering

--- next-js-app/app/users/page.tsx
+++ next-js-app/app/users/page.tsx
@@ -6,12 +6,15 @@ interface User {
 }
 
 const usersPage = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+
+  {/* Disabling caching to make it Dynamic rendered instead of static rendered */}
+  const response = await fetch("https://jsonplaceholder.typicode.com/users", {cache: 'no-store'})
   const users: User[] = await response.json()
 
   return (
     <div>
       <h1>Users</h1>
+      <div>{new Date().toLocaleString()}</div>
       <ul>
         {users.map(user => <li key={user.id}>{user.name}</li>)}
       </ul>
